2ab3155475283d343b2df7d95238d5224772c0fa
RobinKroesen <74556141+RobinKroesen@users.noreply.github.com>
2021-05-26 15:26:07 +0200

    Eerste versie van Stap

---
+++ Stap.java
@@ -0,0 +1,24 @@
+package ShortestPath;
+
+public class Stap {
+    String naam;
+    Integer duur;
+
+    public Integer getDuur() {
+        return duur;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setDuur(Integer duur) {
+        this.duur = duur;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+
+}
