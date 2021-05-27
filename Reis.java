e916073f511233813e5d04a672c61abd82b1ada3
RobinKroesen <74556141+RobinKroesen@users.noreply.github.com>
2021-05-27 20:44:37 +0200

    reis werkende gemaakt

--- Reis.java
+++ Reis.java
@@ -1,53 +1,59 @@
 package ShortestPath;
 
-import java.util.ArrayList;
+import java.util.*;
 
-public class Reis {
+public class Reis{
+    public int kortsteLengte;
+    public ArrayList<Character> bezochtePunten;
 
-    public static void reis(ArrayList<Stap> stappen, Character startPunt, Character eindPunt) {
+    public void Reis(int kortsteLengte, ArrayList<Character> bezochtePunten){
+        this.kortsteLengte = kortsteLengte;
+        this.bezochtePunten = bezochtePunten;
+    }
+
+    public static void reis(ArrayList<Stap> stappen, Character startPunt, Character eindPunt){
         ArrayList<Character> bezochtePunten = new ArrayList<>();
-        ArrayList<Stap> bezochteStappen = new ArrayList<>();
+        ArrayList<Character> eindPunten = new ArrayList<>();
+        ArrayList<Integer> kortstePad = new ArrayList<Integer>();
+        Map<Integer, ArrayList<Character>> resultaten = new HashMap<>();
+
         Character huidigPunt = startPunt;
-        int huidigPad = 0;
-        int kortstePad = 0;
+        int huidigLengte = 0;
+        int totaleLengte = 0;
         for (int i = 0; 1 < stappen.size(); i++){
             if (i == 10){break;}
 
             Stap beginStap = stappen.get(i);
-            //System.out.println(i);
-            //System.out.println(beginStap.naam);
 
             if (startPunt == beginStap.naam){
                 bezochtePunten.add(beginStap.naam);
 
-                    //System.out.println(huidigPunt);
                 while (huidigPunt != eindPunt){
                     for (int u = 0; 1 < stappen.size(); u++){
                         if (u == 10){break;}
                         Stap volgendeStap = stappen.get(u);
 
-                        if (volgendeStap.naam == beginStap.doel) {
-                            //System.out.println(beginStap.lengte);
-                            huidigPad += beginStap.lengte;
-                            //System.out.println(huidigPad);
+                        if ((volgendeStap.naam == beginStap.doel) & !bezochtePunten.contains(volgendeStap.doel)) {
+                            huidigLengte += beginStap.lengte;
                             beginStap = volgendeStap;
                             huidigPunt = beginStap.naam;
                             bezochtePunten.add(huidigPunt);
-
                         }
                     }
                 }
-                kortstePad = huidigPad;
-                huidigPunt = 0;
-                huidigPad = 0;
-                System.out.println(kortstePad);
-
-                System.out.println(bezochtePunten);
+                totaleLengte = huidigLengte;
+                huidigPunt = startPunt;
+                kortstePad.add(totaleLengte);
+                eindPunten = (ArrayList)bezochtePunten.clone();
+                resultaten.put(totaleLengte, eindPunten);
                 bezochtePunten.clear();
+                huidigLengte = 0;
 
             }
         }
-
+        int minAfstand = Collections.min(kortstePad);
+        System.out.println("Het kortste pad is: " + minAfstand + "\nEn komt langs de punten: "+ resultaten.get(minAfstand));
         }
-    }
+
+}
 
